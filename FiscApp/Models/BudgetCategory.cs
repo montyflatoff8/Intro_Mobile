72e725cf060d148ed3281ff37d149efecdba9801
Monty <monty.flatoff@gmail.com>
2026-09-12 22:08:04 -0500

    Started Budget page and page model

---
+++ FiscApp/Models/BudgetCategory.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+
+namespace FiscApp.Models
+{
+    public class BudgetCategory : INotifyPropertyChanged
+    {
+        private string name;
+
+        private decimal monthlyLimit;
+
+        private decimal amountSpent;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public System.Drawing.Color StatusColor
+        {
+            get
+            {
+                if (PercentageUsed >= 1.0)
+                {
+                    return System.Drawing.Color.Red;
+                }
+                else if(PercentageUsed >= 0.8)
+                {
+                    return System.Drawing.Color.Orange;
+                }
+                else
+                {
+                    return System.Drawing.Color.Green;
+                }
+            }
+        }
+
+        public double PercentageUsed
+        {
+            get
+            {
+                return MonthlyLimit == 0 ? 0 : (double)(AmountSpent / MonthlyLimit);
+            }
+        }
+
+
+        public decimal AmountSpent
+        {
+            get
+            {
+                return this.amountSpent;
+            }
+            set
+            {
+                this.amountSpent = value;
+                OnPropertyChanged();
+                NotifyDependentProperties();
+            }
+        }
+
+        public decimal MonthlyLimit
+        {
+            get
+            {
+                return this.monthlyLimit;
+            }
+            set
+            {
+                this.monthlyLimit = value;
+                OnPropertyChanged();
+                NotifyDependentProperties();
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return this.Name;
+            }
+            set
+            {
+                this.Name =  value;
+                OnPropertyChanged();
+            }
+        }
+
+        private void NotifyDependentProperties()
+        {
+            OnPropertyChanged(nameof(PercentageUsed));
+            OnPropertyChanged(nameof(StatusColor));
+        }
+
+
+        protected void OnPropertyChanged ([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ FiscApp/Pages/Budget.xaml.cs
@@ -0,0 +1,9 @@
+namespace FiscApp.Pages;
+
+public partial class Budget : ContentPage
+{
+	public Budget()
+	{
+		InitializeComponent();
+	}
+}
